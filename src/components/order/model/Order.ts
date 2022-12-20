f722c564ccc293c5271bde74eaa5093fc58347d3
AmirAliEidivandi <amirah648887@gmail.com>
2022-12-20 21:41:54 +0330

    refactor orders model

--- src/components/order/model/Order.ts
+++ src/components/order/model/Order.ts
@@ -4,42 +4,15 @@ import orderLineSchema from "./OrderLine";
 import OrderStatus from "./OrderStatus";
 
 const orderSchema: Schema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    total_price: {
-        type: Number,
-        required: true,
-    },
-    coupon: {
-        type: Object,
-        default: null,
-    },
-    final_price: {
-        type: Number,
-        required: true,
-    },
-    order_lines: {
-        type: [orderLineSchema],
-    },
-    status: {
-        type: OrderStatus,
-        required: true,
-        default: OrderStatus.INIT,
-    },
-    delivery_address: {
-        type: Object,
-        default: null,
-    },
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now,
-    },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    totalPrice: { type: Number, required: true },
+    finalPrice: { type: Number, required: true },
+    orderLines: { type: [orderLineSchema] },
+    deliveryAddress: { type: Object, required: true },
+    coupon: { type: Object, default: null },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    status: { type: OrderStatus, required: true, default: OrderStatus.INIT },
 });
 
 export default model<IOrder>("Order", orderSchema);
--- src/components/users/model/User.ts
+++ src/components/users/model/User.ts
@@ -3,39 +3,15 @@ import addressSchema from "./Address";
 import IUser from "./IUser";
 
 const userSchema: Schema = new Schema({
-    first_name: {
-        type: String,
-        required: true,
-    },
-    last_name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    mobile: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    total_orders: {
-        type: Number,
-        default: 0,
-    },
-    wallet: {
-        type: Number,
-        default: 0,
-    },
-    addresses: {
-        type: [addressSchema],
-    },
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
+    totalOrders: { type: Number, default: 0 },
+    wallet: { type: Number, default: 0 },
+    password: { type: String },
+    addresses: { type: [addressSchema], default: [] },
+    createdAt: { type: Date, default: Date.now },
 });
 
 export default model<IUser>("User", userSchema);

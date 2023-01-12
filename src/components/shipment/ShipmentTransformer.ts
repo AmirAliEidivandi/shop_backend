2f78b1c3929b16ec98963234b293946a36f644db
AmirAliEidivandi <amirah648887@gmail.com>
2023-01-12 13:38:12 +0330

    create shipments list

---
+++ src/components/shipment/ShipmentTransformer.ts
@@ -0,0 +1,42 @@
+import DateService from "../../services/DateService";
+import ITransformer from "../contracts/ITransformer";
+import IShipment from "./model/IShipment";
+
+export default class ShipmentTransformer implements ITransformer<IShipment> {
+    private readonly dateService: DateService;
+    constructor() {
+        this.dateService = new DateService();
+    }
+    
+    public transform = (item: IShipment) => {
+        return {
+            id: item._id,
+            employee: this.getEmployee(item.employee),
+            order: {
+                id: this.getOrder(item.order),
+            },
+            selectedDateTime: this.dateService.toPersian(item.selectedDateTime.toUTCString()),
+            deliveredAt: this.dateService.toPersian(item.deliveredAt.toUTCString()),
+            note: item.note,
+            status: item.status,
+        };
+    };
+
+    public collection = (items: IShipment[]) => {
+        return items.map((item: IShipment) => this.transform(item));
+    };
+
+    private getEmployee = (user: any) => {
+        if (!user) {
+            return null;
+        }
+        return {
+            firstName: user.firstName,
+            lastName: user.lastName,
+        };
+    };
+
+    private getOrder = (order: any) => {
+        return order._id;
+    };
+}
